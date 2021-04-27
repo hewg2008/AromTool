0c70ad4f2743db3ca5267facfd634f646f8042c2
Wengan He <38550848+hewg2008@users.noreply.github.com>
2021-04-27 19:58:22 +0800

    Change path

--- example/calculate.py
+++ example/calculate.py
@@ -2,7 +2,7 @@ from aromtool.calculator import EnergyCalculator
 from ase.io import read
 
 # Access the coordinate data from ring file.
-xyz_contact = read(r'.\contact\7std_contact_4.xyz')
+xyz_contact = read(r'2xde_contact_1.xyz')
 
 # Choose a model
 pinn = ('AromNN-PiBPTT.pb')
